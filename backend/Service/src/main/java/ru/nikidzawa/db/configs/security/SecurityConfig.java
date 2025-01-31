40d9c2847a4d9dfb0872d1ae24266dae8153ba3a
Nikidzawa <datr1932@gmail.com>
2025-01-31 06:44:35 +0300

    Исправления в авторизации (Бекенд)

--- backend/Service/src/main/java/ru/nikidzawa/db/configs/security/SecurityConfig.java
+++ backend/Service/src/main/java/ru/nikidzawa/db/configs/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("users/login", "users/existsBy", "users/save", "mail/**").permitAll()
+                        .requestMatchers("users/**", "mail/**", "chats/getAllUserChats/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
